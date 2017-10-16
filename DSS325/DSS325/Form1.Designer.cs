1d90340a64ab4813c82b8ad50710d4e3c6232720
Zander van Vuuren <24678317@student.g.nwu.ac.za>
2017-10-16 15:40:22 +0200

    Added the basic program
    it randomizes numbers for us.

---
+++ DSS325/DSS325/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace DSS325
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(589, 353);
+            this.panel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(642, 286);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(854, 441);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ DSS325/DSS325/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSS325
+{
+    public partial class Form1 : Form
+    {
+        private int[] averageRainFall;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            averageRainFall = new int[12] {1,2,3,4,5,6,7,8,9,10,11,12};
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 13;i++)
+            {
+                MessageBox.Show(Convert.ToString(getRainFall(averageRainFall[i])));
+            }
+            
+        }
+
+        private double getRainFall(int iMonth)
+        {
+            switch (iMonth)
+            {
+                case 1:
+                    return monthRandomizer(18,3);           //January
+                    break;
+                case 2:
+                    return monthRandomizer(16, 1);          //February
+                    break;
+                case 3:
+                    return monthRandomizer(48, 6);          //March
+                    break;
+                case 4:
+                    return monthRandomizer(62, 4);          //April
+                    break;
+                case 5:
+                    return monthRandomizer(109, 17);        //May
+                    break;
+                case 6:
+                    return monthRandomizer(219, 110);       //June
+                    break;
+                case 7:
+                    return monthRandomizer(143, 51);        //July
+                    break;
+                case 8:
+                    return monthRandomizer(119, 48);        //August   
+                    break;
+                case 9:
+                    return monthRandomizer(57, 29);         //September
+                    break;
+                case 10:
+                    return monthRandomizer(14, 9);          //October
+                    break;
+                case 11:
+                    return monthRandomizer(58, 9);          //November  
+                    break;
+                case 12:
+                    return monthRandomizer(30, 6);          //December          
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;      
+            }
+            return 0;
+        }
+
+        private double monthRandomizer(int max, int min)
+        {
+            return ((random.NextDouble() * (max - min)) + min);
+        }
+    }
+}
